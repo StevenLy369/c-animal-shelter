83e532c4be0c2e66c9ca6bd3f7bbf0b8a9f0fba9
Steven Ly <1stevenly92@gmail.com>
2020-05-27 15:55:24 -0700

    able to delete animals from list

--- AnimalShelter.Solutions/Controllers/AnimalsController.cs
+++ AnimalShelter.Solutions/Controllers/AnimalsController.cs
@@ -5,38 +5,52 @@ using System.Linq;
 
 namespace AnimalShelter.Controllers
 {
-  public class AnimalsController : Controller
-  {
-    private readonly AnimalContext _db;
-
-    public AnimalsController(AnimalContext db)
+    public class AnimalsController : Controller
     {
-      _db = db;
-    }
+        private readonly AnimalContext _db;
 
-    public ActionResult Index()
-    {
-      List<Animal> model = _db.Animals.ToList();
-      return View(model);
-    }
+        public AnimalsController(AnimalContext db)
+        {
+            _db = db;
+        }
 
-    public ActionResult Create()
-    {
-      return View();
-    }
+        public ActionResult Index()
+        {
+            List<Animal> model = _db.Animals.ToList();
+            return View(model);
+        }
 
-    [HttpPost]
-    public ActionResult Create(Animal animal)
-    {
-      _db.Animals.Add(animal);
-      _db.SaveChanges();
-      return RedirectToAction("Index");
-    }
+        public ActionResult Create()
+        {
+            return View();
+        }
 
-    public ActionResult Details(int id)
-    {
-      Animal thisAnimal = _db.Animals.FirstOrDefault(animals => animals.AnimalId == id);
-      return View(thisAnimal);
+        [HttpPost]
+        public ActionResult Create(Animal animal)
+        {
+            _db.Animals.Add(animal);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Details(int id)
+        {
+            Animal thisAnimal = _db.Animals.FirstOrDefault(animals => animals.AnimalId == id);
+            return View(thisAnimal);
+        }
+        public ActionResult Delete(int id)
+        {
+            var thisAnimal = _db.Animals.FirstOrDefault(animals => animals.AnimalId == id);
+            return View(thisAnimal);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var thisAnimal = _db.Animals.FirstOrDefault(animals => animals.AnimalId == id);
+            _db.Animals.Remove(thisAnimal);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
-  }
 }
--- AnimalShelter.Solutions/Models/Animal.cs
+++ AnimalShelter.Solutions/Models/Animal.cs
@@ -10,6 +10,10 @@ namespace AnimalShelter.Models
     private static List<Animal> _instances = new List<Animal> { };
     public int AnimalId { get; set; }
     public string Type { get; set; } 
+
+    public string Gender {get;set;}
+    // public int Admittance {get;set;}
+    public string Name {get;set;}
   }
 
 }
